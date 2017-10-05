46cc716ddc550b8259e73672304dfaf71536ef9f
perro-negro <gi.vega@itsur.edu.mx>
2017-10-05 12:03:46 -0500

    se agrego el dao de contactos y se implemento insert y gettalll

---
+++ MiSQLiteEnAndroidB/app/src/main/java/modelo/Contacto.java
@@ -0,0 +1,79 @@
+package modelo;
+
+import java.util.Date;
+
+/**
+ * Created by alcohonsilver on 05/10/17.
+ */
+
+class Contacto {
+
+
+    private long  id;
+
+    public Contacto(long id, String nombre, String correo_electronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo_electronico = correo_electronico;
+    }
+
+    public Contacto() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    String nombre;
+    String correo_electronico;
+    String twitter;
+    String telefono;
+    Date fecha_nacimiento;
+
+}
+
--- MiSQLiteEnAndroidB/app/src/main/java/modelo/MiSQLiteOpenHelper.java
+++ MiSQLiteEnAndroidB/app/src/main/java/modelo/MiSQLiteOpenHelper.java
@@ -10,16 +10,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MiSQLiteOpenHelper extends SQLiteOpenHelper {
 
-    private  final String SCRIPT_TABLE_CONTACTOS="create table CONTACTOS (_id)";
+    public static final String  TABLE_CONTACTOS_NAME="contactos";
+    public static final String[] COLUMNS_NAME_TABLE_CONTACTOS = {"_id", "nombre","correo_electronico"
+    , "twitter","telefono","fecha_nacimiento"};
+
+    private  final String SCRIPT_TABLE_CONTACTOS="create table " + TABLE_CONTACTOS_NAME + "(" +
+              COLUMNS_NAME_TABLE_CONTACTOS[0] + " integer primary key autoincrement, " +
+            COLUMNS_NAME_TABLE_CONTACTOS[1] + " varchar(100) not null, " +
+            COLUMNS_NAME_TABLE_CONTACTOS[2] + " varchar(200) not null, " +
+            COLUMNS_NAME_TABLE_CONTACTOS[3] + " varchar(100) null, " +
+            COLUMNS_NAME_TABLE_CONTACTOS[4] + " varchar(20) null, " +
+            COLUMNS_NAME_TABLE_CONTACTOS[5] + " date null " +
+            ");";
     private static final int VERSION_DB = 1;
 
     public MiSQLiteOpenHelper(Context contexto){
+
         super(contexto, "miBD", null, VERSION_DB);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(SCRIPT_TABLE_CONTACTOS);
     }
 
     @Override
