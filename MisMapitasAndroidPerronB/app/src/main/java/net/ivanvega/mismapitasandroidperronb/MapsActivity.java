11eb16ee09c7b7ad91db12910f9e4de99a82c033
perro-negro <gi.vega@itsur.edu.mx>
2018-03-13 15:43:55 -0600

    Se agrego mapas y localizacion al proyecto. NOTA: No se validan los permisos en tiempo de ejecucuon

--- MisMapitasAndroidPerronB/app/src/main/java/net/ivanvega/mismapitasandroidperronb/MapsActivity.java
+++ MisMapitasAndroidPerronB/app/src/main/java/net/ivanvega/mismapitasandroidperronb/MapsActivity.java
@@ -2,27 +2,46 @@ package net.ivanvega.mismapitasandroidperronb;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback, View.OnClickListener {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
+
+    private Button btnOpcion, btnAnimar, btnMOver, btnPOsicion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        inicializarUI();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
+
+
+    private void inicializarUI(){
+        btnOpcion = (Button) findViewById(R.id.btnOpcion); btnOpcion.setOnClickListener(this);
+        btnAnimar = (Button) findViewById(R.id.btnAnimar); btnAnimar.setOnClickListener(this);
+        btnMOver = (Button)findViewById(R.id.btnMover); btnMOver.setOnClickListener(this);
+        btnPOsicion = (Button) findViewById(R.id.btnPosc); btnPOsicion.setOnClickListener(this);
+    }
+
 
 
     /**
@@ -36,12 +55,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Perro-negro in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
+        map.addMarker(new MarkerOptions().position(sydney).title("Perro-negro in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.equals(btnOpcion)){
+            map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            map.getUiSettings().setZoomControlsEnabled(true);
+        }
+
+        if(v.equals(btnMOver)){
+
+            CameraUpdate camera = CameraUpdateFactory.
+                    newLatLngZoom(new LatLng(19.427304,-99.137822),16);
+
+            map.moveCamera(camera);
+        }
+
+        if(v.equals(btnAnimar)){
+            LatLng angelInd = new LatLng(19.426978, -99.167775);
+            CameraPosition position = new CameraPosition.Builder()
+                    .target(angelInd)
+                    .bearing(45)
+                    .zoom(19)
+                    .tilt(70)
+                    .build();
+            CameraUpdate campos = CameraUpdateFactory.newCameraPosition(position);
+            map.animateCamera(campos);
+        }
+
+        if(v.equals(btnPOsicion)){
+            CameraPosition camPosition = map.getCameraPosition();
+            Toast.makeText(this, "Lat: " + String.valueOf(camPosition.target.latitude) +
+                            "\nLon: " + String.valueOf(camPosition.target.longitude )+
+                    "\nbearing: " + String.valueOf(camPosition.bearing) +
+                                    "\ntilt: " + String.valueOf(camPosition.tilt
+                    )
+                    , Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
